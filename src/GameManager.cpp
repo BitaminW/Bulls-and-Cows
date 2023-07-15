df819c6159e5d490e345144ae946a20c3b834fe9
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-07-15 20:35:43 +0900

    Update GameManager.cpp

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -16,31 +16,67 @@ GameManager* GameManager::GetInstance(){
 }
 
 void GameManager::InitGame(){
-
+	system(WINDOW_SIZE);
 	
 }
 
 
 
 void GameManager::DrawMenu(){
-	const char* Menu[7][14] = {
-		{"■■",   "    ■  ■", "  ■", "      ■", "      ■■■"},
-		{"■  ■", "  ■  ■", "  ■", "      ■", "      ■"},
-		{"■  ■", "  ■  ■", "  ■", "      ■", "      ■"},
-		{"■■",   "    ■  ■", "  ■", "      ■", "      ■■■"},
-		{"■  ■", "  ■  ■", "  ■", "      ■", "          ■"},
-		{"■  ■", "  ■  ■", "  ■", "      ■", "          ■"},
-		{"■■",   "    ■■■", "  ■■■", "  ■■■", "  ■■■"}
+	const char* Menu[7][12] = {
+		{"■■",   "   ■  ■", " ■", "     ■", "     ■■■", "", "", "", "                       ■■■■", " ■■■", " ■          ■", " ■■■"},
+		{"■  ■", " ■  ■", " ■", "     ■", "     ■", "", "", "                        ■", " ■    ■", " ■  ■", " ■          ■", " ■"},
+		{"■  ■", " ■  ■", " ■", "     ■", "     ■", "      ■■■", "", "            ■", " ■", "       ■  ■", " ■    ■    ■", " ■"},
+		{"■■",   "   ■  ■", " ■", "     ■", "     ■■■", "      ■", " ■■■", "     ■", " ■", "       ■  ■", " ■   ■■   ■", " ■■■"},
+		{"■  ■", " ■  ■", " ■", "     ■", "         ■", "  ■■■", " ■  ■", " ■■■", " ■", "       ■  ■", " ■  ■  ■  ■", "     ■"},
+		{"■  ■", " ■  ■", " ■", "     ■", "         ■", "  ■  ■", " ■  ■", " ■  ■", " ■    ■", " ■  ■", " ■ ■    ■ ■", "     ■"},
+		{"■■",   "   ■■■", " ■■■", " ■■■", " ■■■", "  ■■■", " ■  ■", " ■■■", " ■■■■", " ■■■", " ■■      ■■", " ■■■"}
 	};
 
 
 	for (int i = 0; i < 7; i++) {
-		for (int j = 0; j < 14; j++) {
+		for (int j = 0; j < 12; j++) {
 			
 			switch (j){
 			case 0:
-				RED
-					break;
+				RED;
+				break;
+			case 1:
+				GREEN;
+				break;
+			case 2:
+				BLUE_GREEN;
+				break;
+			case 3:
+				PURPLE;
+				break;
+			case 4:
+				GOLD;
+				break;
+			case 5:
+				PLUM;
+				break;
+			case 6:
+				YELLOW;
+				break;
+			case 7:
+				WHITE;
+				break;
+			case 8:
+				GRAY;
+				break;
+			case 9:
+				SKY_BLUE;
+				break;
+			case 10:
+				BLOOD;
+				break;
+			case 11:
+				DARK_BLUE;
+				break;
+			case 12:
+				HIGH_GREEN;
+				break;
 			}
 			printf("%s", Menu[i][j]);
 		}
