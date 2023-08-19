afdddb44eb195389442eb05260c6bf6711d7f72b
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-08-19 19:55:02 +0900

    Create SecretNumber.h

---
+++ Header/SecretNumber.h
@@ -0,0 +1,13 @@
+#pragma once
+class SecretNumber{
+public:
+	
+	friend std::istream& operator>>(std::istream& in,string str) {
+
+	}
+
+	friend std::ostream& operator<<(std::ostream& out,const string str) {
+
+	}
+};
+
