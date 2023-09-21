b351740f7f9e578b2a480c2faa4124fda08eb01c
BitaminW <114838001+BitaminW@users.noreply.github.com>
2023-09-21 12:25:01 +0900

    Update Macro.h

--- Header/Macro.h
+++ Header/Macro.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define COL					         GetStdHandle(STD_OUTPUT_HANDLE)
+#define COL    GetStdHandle(STD_OUTPUT_HANDLE)
 
-#define BLACK                SetConsoleTextAttribute(COL, 0x0000)        
-#define DARK_BLUE			       SetConsoleTextAttribute(COL, 0x0001)      
-#define GREEN                SetConsoleTextAttribute(COL, 0x0002)        
+#define BLACK    SetConsoleTextAttribute(COL, 0x0000)        
+#define DARK_BLUE    SetConsoleTextAttribute(COL, 0x0001)      
+#define GREEN    SetConsoleTextAttribute(COL, 0x0002)        
 #define BLUE_GREEN           SetConsoleTextAttribute(COL, 0x0003)        
 #define BLOOD                SetConsoleTextAttribute(COL, 0x0004)        
 #define PURPLE               SetConsoleTextAttribute(COL, 0x0005)        
